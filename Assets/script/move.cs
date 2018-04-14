c5beaf87ea48ed6644a447d51f645ef52ad112b9
Len_AshBell <jason1022liu@gmail.com>
2018-04-14 19:05:09 +0800

    [project] set charactor move

---
+++ Assets/script/move.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+	Rigidbody2D player;
+	Animator player_anim;
+  
+
+	// Use this for initialization
+	void Start () {
+		player_anim = GetComponent<Animator> ();
+    player = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    player_anim.SetFloat ("stayControl", 0);
+    
+		if(Input.GetKey(KeyCode.UpArrow)) {
+			player.velocity = new Vector2(0, 5);
+      // transform.Translate(new Vector2(0, 0.1f));
+			player_anim.SetFloat ("walkControlX", 0);
+      player_anim.SetFloat ("walkControlY", 1);
+      player_anim.SetFloat ("stayControl", 1);
+		}
+			
+		if(Input.GetKey(KeyCode.DownArrow)){
+			player.velocity = new Vector2(0, -5);
+      // transform.Translate(new Vector2(0, -0.1f));
+			player_anim.SetFloat ("walkControlX", 0);
+      player_anim.SetFloat ("walkControlY", -1);
+      player_anim.SetFloat ("stayControl", 1);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			player.velocity = new Vector2(-5, 0);
+      // transform.Translate(new Vector2(-0.1f, 0));
+      player_anim.SetFloat ("walkControlX", -1);
+      player_anim.SetFloat ("walkControlY", 0);
+      player_anim.SetFloat ("stayControl", 1);
+		}
+		if(Input.GetKey(KeyCode.RightArrow)){
+			player.velocity = new Vector2(5, 0);
+      // transform.Translate(new Vector2(0.1f, 0));
+      player_anim.SetFloat ("walkControlX", 1);
+      player_anim.SetFloat ("walkControlY", 0);
+      player_anim.SetFloat ("stayControl", 1);
+		}
+
+    
+
+    
+
+    
+	}
+
+
+}
